d4c295231948a33f9ee8efce36bb68dacf3f3ca4
rosergenbg-debug <roserge.nbg@gmail.com>
2026-08-21 16:54:53 +0200

    Bump Logo to v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rosergenbg.logo"
         minSdk = 33
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
