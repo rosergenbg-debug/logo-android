19b64fbceab19c57ba0de3d56abc4749af39a5c0
rosergenbg-debug <roserge.nbg@gmail.com>
2026-08-21 15:45:32 +0200

    v1.1: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rosergenbg.logo"
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
